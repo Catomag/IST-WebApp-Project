0be8b46abbafb2d48482126ef1aaa637522f1e01
Catomag <48117609+Catomag@users.noreply.github.com>
2019-06-25 23:07:54 +1000

    Added more interaction code for the server and client.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+//Dependencies
 const express = require("express");
 const path = require("path");
 const port = 3000;
 
+
+//Sets up the server
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -11,10 +14,65 @@ app.listen(port, () => {
   console.log("Server listening on port " + port);
 });
 
+
+//Host variables
+
+var hostTemplate = {
+  id: "",
+  question: "",
+  playerCount: 0,
+  votes: []
+}
+
+var hosts = [];
+
+
+//Get requests (Requests visible in the url)
+
+//Standard response
 app.get("/", (req, res) => {
   res.render("index.pug");
 });
 
-app.get("/id/:id/", (req, res) => {
+app.get("/:id/", (req, res) => {
 
 });
+
+
+//Post requests (Hidden requests)
+
+//Creates a new game and returns id
+app.post("/create/:question/", (req, res) => {
+  var temp = new hostTemplate;
+  temp.question = req.params.question;
+  temp.id = makeid(12);
+  /*var votes = JSON.parse(req.params.votes);
+  for (var i = 0; i < votes.length; i++) {
+    temp.votes.push(votes[i]);
+  }*/
+  res.send(temp.id);
+});
+
+//Gets info from existing id
+app.post("/id/:id/", (req, res) => {
+  var id = req.params.id;
+  for(var i = 0; i < hosts.length; i++) {
+    if(hosts[i].id == id) {
+      res.send(JSON.stringify(hosts[i]));
+    }
+  }
+  res.send("null");
+});
+
+
+//Helper functions
+
+function makeid(length) {
+   var result = '';
+   var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
+   for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
--- public/js/formSubmit.js
+++ public/js/formSubmit.js
@@ -1,8 +1,19 @@
 var questionInput = document.getElementById("questionInput");
 
-
 function sendToServer() {
-  var url = window.location.host + "/";
-  fetch();
+  console.log("text input " + questionInput.value);
+  var url = window.location.host + "/create/" + questionInput.value;
+  var request = new Request(url, {
+    method: 'POST',
+    body: data,
+    headers: new Headers()
+  });
+
+  fetch(request)
+  .then((data) => {
+    console.log("this ran");
+    console.log(data.id);
+    document.getElementById("idThing").value = data.id;
+  });
   return false;
 }
